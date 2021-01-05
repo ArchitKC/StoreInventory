c949f61c222c097a5925bfa1b0e9cfe61c856d8d
ArchitKC <archithrd@gmail.com>
2021-01-05 13:26:59 -0500

    Final Unit Tests

---
+++ StoreInventory.Services.Test/ProductServicesTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using StoreInventory.Models.ViewModel;
+using StoreInventory.Services.Interfaces;
+
+namespace StoreInventory.Services.Test
+{
+    [TestClass]
+    public class ProductServicesTest
+    {
+        [TestInitialize]
+        public void getProductView()
+        {
+            List<ProductDetailsViewModel> inventoryExpected = new List<ProductDetailsViewModel> {
+                    new ProductDetailsViewModel
+                    {
+                        Id= 1,
+                        Product = "Shirt"
+                    },
+                    new ProductDetailsViewModel
+                    {
+                        Id=2,
+                        Product = "Pant"
+                    }
+                };
+        }
+        [TestMethod]
+        public void GetMethodwithNull()
+        {
+            int Id = 1;
+
+            Mock<IProductServices> mockItem = new Mock<IProductServices>();
+            mockItem.Setup(t =>t.getProducts(It.IsAny<int>())).
+                    Returns(new ProductDetailsViewModel() { Id = 1, Product = "Shirt" });
+
+            ProductServices productServices = new ProductServices();
+            var getItem = productServices.getProducts();
+
+            Assert.IsTrue(getItem.Count == 0);
+        }
+    }
+}
---
+++ StoreInventory.Services.Test/UserDetailsServicesTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StoreInventory.Services.Test
+{
+    [TestClass]
+    public class UserDetailsServicesTest
+    {
+        [TestMethod]
+        public void CreditCardNumberIsNull()
+        {
+            string creditCardNumber = string.Empty;
+            int id = 1;
+            UserDetailsServices userDetailsServices = new UserDetailsServices();
+            bool bCard = userDetailsServices.ChargePayment(creditCardNumber, id);
+            Assert.AreEqual(bCard,false);
+        }
+
+        [TestMethod]
+        public void CreditCardNumberIsNotNull()
+        {
+            string creditCardNumber = "1233452342342345";
+            int id = 1;
+            UserDetailsServices userDetailsServices = new UserDetailsServices();
+            bool bCard = userDetailsServices.ChargePayment(creditCardNumber, id);
+            Assert.AreEqual(bCard, true);
+        }
+
+        [TestMethod]
+        public void CreditCardNumberLength16()
+        {
+            string creditCardNumber = "1233452342342345";            
+            Assert.AreEqual(creditCardNumber.Length, 16);
+        }
+
+    }
+}
